fc1b806d2db01ec00342a42f0f0f79c82a799c0e
Jammer <jammer.08123@gmail.com>
2024-10-17 15:53:29 -0600

    se crearon los endpoints

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import "dotenv/config.js"
 import "./config/dataBase.js"
+import cors from 'cors'
+import morgan from 'morgan';
+import indexRouter from './router/index.js'
 
 const server= express ()
 const PORT= process.env.PORT
 const Ready= ()=> console.log("El servidor esta listo en el puerto:"+PORT);
+
+server.use(express.json())                        //permite leer json
+server.use(express.urlencoded({extended:true}))   //permite leer formularios
+server.use(cors())                                //es la parte de seguridad
+server.use(morgan('dev'))                         //ayuda a ver las peticiones que se hacen
+server.use('/shops', indexRouter)                 //hace uso del enrutador router
 
 server.listen(PORT, Ready)
